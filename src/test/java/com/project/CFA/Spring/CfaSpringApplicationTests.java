260499e9db6e613771d87e3a5cc1f60f10262df9
tddixon0 <119270651+tddixon0@users.noreply.github.com>
2024-11-12 22:30:00 -0500

    first commit

---
+++ src/test/java/com/project/CFA/Spring/CfaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.CFA.Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
